b4dcac5b454aa92f087cf3ca28d85bdeb72663a5
kamilstalinski <kamstal95@gmail.com>
2022-12-14 13:32:22 +0100

    initial setup

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import Navbar from "../components/Navbar";
+
+export default function Home() {
+  return (
+    <div className='Home'>
+      <Navbar />
+      <div className='left-hero'></div>
+      <div className='right-hero'></div>
+    </div>
+  );
+}
